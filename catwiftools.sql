c17fb43b57259f4a930f71ca880fbe62cf9179ae
theo-bggtt <theo.bggtt@eduge.ch>
2025-01-10 15:06:42 +0100

    Update catwiftools.sql
    Clean sql

--- catwiftools.sql
+++ catwiftools.sql
@@ -34,27 +34,6 @@ CREATE TABLE `wallets` (
   `walletType` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Déchargement des données de la table `wallets`
---
-
-INSERT INTO `wallets` (`idWallet`, `walletName`, `walletphrase`, `walletType`) VALUES
-(31, 'Wallet-1', 'hero tunnel stumble into loop riot depth walnut love tragic private ship', 'bundle'),
-(32, 'Wallet-2', 'canvas patient lemon unfair rifle depart put ridge beach suspect head dance', 'bundle'),
-(33, 'Wallet-3', 'frost oblige leader cupboard trap toy upgrade olive magnet panel garment tortoise', 'bundle'),
-(34, 'Wallet-4', 'early forget dress ridge license chuckle math before harvest pulse width tape', 'bundle'),
-(35, 'Wallet-5', 'sample abuse permit glove tiger appear subject zone little tragic panther learn', 'bundle'),
-(36, 'Wallet-1', 'above culture current uncover armor ahead stand apart lens believe impact real', 'bundle'),
-(37, 'Wallet-2', 'vacuum argue zebra few village volume country animal ridge topic lab talk', 'bundle'),
-(38, 'Wallet-3', 'poet circle spring brass slight rug alarm usual action assume library copper', 'bundle'),
-(39, 'Wallet-4', 'tree tribe section feed pluck illness guess click room term harvest solve', 'bundle'),
-(40, 'Wallet-5', 'seed method east denial minute route bundle hawk radio casino saddle that', 'bundle'),
-(41, 'Wallet-1', 'spoon outside fatal peanut police capital unfair arrange very demise such dumb', 'bundle'),
-(42, 'Wallet-2', 'venture bird salmon joke post cruel train page hard summer october trial', 'bundle'),
-(43, 'Wallet-3', 'random hen outdoor thing chase setup public prison silk envelope leave ignore', 'bundle'),
-(44, 'Wallet-4', 'cradle image fresh great soldier filter toss carry sadness purse antique choose', 'bundle'),
-(45, 'Wallet-5', 'budget bonus orphan recipe other alarm piano develop clay park hill goddess', 'bundle');
-
 -- --------------------------------------------------------
 
 --
@@ -72,6 +51,7 @@ CREATE TABLE `wallettypes` (
 
 INSERT INTO `wallettypes` (`idType`, `typeName`) VALUES
 (1, 'bundle');
+(2, 'dev');
 
 --
 -- Index pour les tables déchargées
@@ -100,13 +80,13 @@ ALTER TABLE `wallettypes`
 -- AUTO_INCREMENT pour la table `wallets`
 --
 ALTER TABLE `wallets`
-  MODIFY `idWallet` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=46;
+  MODIFY `idWallet` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
 
 --
 -- AUTO_INCREMENT pour la table `wallettypes`
 --
 ALTER TABLE `wallettypes`
-  MODIFY `idType` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `idType` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Contraintes pour les tables déchargées
@@ -116,7 +96,7 @@ ALTER TABLE `wallettypes`
 -- Contraintes pour la table `wallets`
 --
 ALTER TABLE `wallets`
-  ADD CONSTRAINT `wallets_ibfk_1` FOREIGN KEY (`walletType`) REFERENCES `wallettypes` (`typeName`);
+  ADD CONSTRAINT `fk_walettype` FOREIGN KEY (`walletType`) REFERENCES `wallettypes` (`typeName`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
